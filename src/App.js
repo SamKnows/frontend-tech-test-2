75651044009f361ec5254440dad7302a5b868113
Jarryd Crawford <jarrydcrawford@gmail.com>
2017-03-06 10:08:23 +0000

    Add fetches for currency API and add mock components
    - Use fetches to receive data from API
    - Set up basic local state
    - Import and mock out child components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import PriceForm from './components/PriceForm'
+import PriceTable from './components/PriceTable'
+
+import './App.css'
 
 class App extends Component {
+  state = {
+    initialPrices: {},
+    prices: {},
+  }
+
+  componentDidMount() {
+    this.fetchPrices()
+      .then((resp) => this.setState({
+        initialPrices: resp,
+        prices: resp,
+      }))
+  }
+
+  fetchPrices = (base = 'GBP') =>
+    fetch(`http://api.fixer.io/latest?base=${base}`)
+      .then((resp) => resp.json())
+
+  setTablePrices = (base = 'GBP') => {
+    this.fetchPrices(base)
+      .then(resp => this.setState({
+        prices: resp
+      }))
+  }
+
   render() {
+    const { initialPrices, prices } = this.state
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Currency Converter</h1>
+        {!!Object.entries(initialPrices).length &&
+          <PriceForm
+            getPrices={this.setTablePrices}
+            prices={prices}
+            selection={initialPrices}
+          />}
+        {!!Object.entries(prices).length &&
+          <PriceTable rates={prices.rates} />}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
